0fd773e4475f098af003d7f2cc676a2892e524d4
Stuart Mumford <stuart@cadair.com>
2022-05-23 12:11:00 +0100

    minimal example

---
+++ export.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+index=./talk.org
+
+oxreveal=$(find ~/.emacs.d/elpa/28.0/develop/ -type d -name "ox-reveal-*" -print -quit)
+
+progn="(progn
+  (package-initialize)
+  (add-to-list 'load-path \"$oxreveal\")
+  (require 'ox-reveal)
+  (org-reveal-export-to-html))"
+
+emacs --batch --visit "$index" --eval "$progn" --kill
